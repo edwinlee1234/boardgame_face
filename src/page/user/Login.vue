42653dcf1852ac9dbbde4f334ade8d69c212e1ac
edwin_lee <edwin_lee@mail.chungyo.net>
2018-09-13 15:24:55 +0800

    新增會員登入 & 註冊

---
+++ src/page/user/Login.vue
@@ -0,0 +1,51 @@
+<template>
+    <div id="Login">
+      <form>
+        <div class="form-group">
+          <label for="exampleInputEmail1">User Name</label>
+          <input type="text" class="form-control" v-model="userName" placeholder="User Name">
+        </div>
+        <div class="form-group">
+          <label for="exampleInputPassword1">Password</label>
+          <input type="password" class="form-control" v-model="password" placeholder="Password">
+        </div>
+        <button type="button" class="btn btn-primary" @click.passive="login">Submit</button>
+      </form>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: 'Login',
+
+        data () {
+            return {
+              userName: '',
+              password: ''
+            }
+        },
+
+        methods: {
+          login() {
+            axios({
+                method: "post",
+                url: APIURL + '/api/user/login',
+                responseType: 'json',
+                data: {
+                  userName: this.userName,
+                  password: this.password                    
+                }
+            })              
+            .then((response) => {
+              console.log(response);
+            })
+            .catch((error) => {
+              console.log(error);
+            });
+          }
+        }
+    }
+</script>
+
+<style lang="scss" scoped>
+</style>
---
+++ src/page/user/Register.vue
@@ -0,0 +1,57 @@
+<template>
+    <div id="Register">
+      <form>
+        <div class="form-row">
+          <div class="form-group col-md-6">
+            <label for="inputEmail4">User Name</label>
+            <input type="text" class="form-control" v-model="userName" placeholder="Email">
+          </div>
+          <div class="form-group col-md-6">
+            <label for="inputPassword4">Password</label>
+            <input type="password" class="form-control" v-model="password" placeholder="Password">
+          </div>
+        </div>
+        <button type="button" class="btn btn-primary" @click.passive="register">Sign in</button>
+      </form>        
+    </div>
+</template>
+
+<script>
+    export default {
+        name: 'Register',
+
+        data () {
+            return {
+              userName: '',
+              password: ''
+            }
+        },
+
+        mounted() {
+
+        },
+
+        methods: {
+          register() {
+            axios({
+                method: "post",
+                url: APIURL + '/api/user/register',
+                responseType: 'json',
+                data: {
+                  userName: this.userName,
+                  password: this.password                    
+                }
+            })              
+            .then((response) => {
+              console.log(response);
+            })
+            .catch((error) => {
+              console.log(error);
+            });
+          }
+        }
+    }
+</script>
+
+<style lang="scss" scoped>
+</style>
