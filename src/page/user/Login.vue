d37d9284da7fd7c647d04bd689c35f8861757dba
edwin_lee <edwin_lee@mail.chungyo.net>
2018-09-15 15:04:28 +0800

    調查Login & Register Style

--- src/page/user/Login.vue
+++ src/page/user/Login.vue
@@ -1,16 +1,33 @@
 <template>
     <div id="Login">
-      <form>
-        <div class="form-group">
-          <label for="exampleInputEmail1">User Name</label>
-          <input type="text" class="form-control" v-model="userName" placeholder="User Name">
+      <div class="container">
+        <div class="loignBox">
+          <div class="loignForm">
+            <h2>Login</h2>
+            <br>
+            <form>
+              <div class="form-group">
+                <label>User Name</label>
+                <input type="text" class="form-control" v-model="userName" placeholder="User Name">
+              </div>
+              <div class="form-group">
+                <label>Password</label>
+                <input type="password" class="form-control" v-model="password" placeholder="Password">
+              </div>
+              <button type="button" class="btn btn-primary" @click.passive="login">Submit</button>
+            </form>
+          </div>
+          <div class="socialForm">
+            <h3>或者與社群做聯結登入</h3>
+            <div class="fbBox">
+            </div>
+            <div class="googleBox">
+            </div>
+          </div>
         </div>
-        <div class="form-group">
-          <label for="exampleInputPassword1">Password</label>
-          <input type="password" class="form-control" v-model="password" placeholder="Password">
-        </div>
-        <button type="button" class="btn btn-primary" @click.passive="login">Submit</button>
-      </form>
+
+      </div>
+
     </div>
 </template>
 
@@ -52,4 +69,41 @@
 </script>
 
 <style lang="scss" scoped>
+.loignBox{
+  padding: 40px;
+  border: solid 1px;
+  border-radius: 10px;
+  margin-top: 30px;
+  width: 100%;
+
+  button {
+    margin-top: 10px;
+    width: 180px;
+  }
+
+  h3 {
+    margin-bottom: 20px
+  }
+
+  .socialForm {
+    border-top: solid 1px;
+    margin-top: 30px;
+    padding-top: 30px;
+
+    .fbBox, .googleBox {
+      display: inline-block;
+      margin-right: 5px;
+      margin-left: 5px;
+      width: 80px;
+      height: 80px;  
+      background-size:cover; 
+    }
+    .fbBox{
+      background-image: url('/static/images/fb_icon.png');
+    }
+    .googleBox{
+      background-image: url('/static/images/google_icon.jpg');
+    }
+  }
+}
 </style>
--- src/page/user/Register.vue
+++ src/page/user/Register.vue
@@ -1,18 +1,34 @@
 <template>
     <div id="Register">
-      <form>
-        <div class="form-row">
-          <div class="form-group col-md-6">
-            <label for="inputEmail4">User Name</label>
-            <input type="text" class="form-control" v-model="userName" placeholder="Email">
+      <div class="container">
+        <div class="row">
+          <div class="col-6">
+            <div class="registerBox">
+              <h2>註冊</h2>
+              <form>
+                <div class="form-group">
+                  <label>User Name</label>
+                  <input type="text" class="form-control" v-model="userName" placeholder="Email">
+                </div>
+                <div class="form-group">
+                  <label>Password</label>
+                  <input type="password" class="form-control" v-model="password" placeholder="Password">
+                </div>
+                <button type="button" class="btn btn-primary" @click.passive="register">Sign in</button>
+              </form> 
+            </div>
           </div>
-          <div class="form-group col-md-6">
-            <label for="inputPassword4">Password</label>
-            <input type="password" class="form-control" v-model="password" placeholder="Password">
+          <div class="col-6">
+            <div class="socialForm">
+              <h3>或者與社群做聯結註冊</h3>
+              <div class="fbBox">
+              </div>
+              <div class="googleBox">
+              </div>
+            </div>
           </div>
         </div>
-        <button type="button" class="btn btn-primary" @click.passive="register">Sign in</button>
-      </form>        
+      </div>
     </div>
 </template>
 
@@ -57,4 +73,41 @@
 </script>
 
 <style lang="scss" scoped>
+#Register {
+  margin-top: 30px;
+  .registerBox {
+    h2 {
+      margin-bottom: 30px;
+    }
+    button {
+      margin-top: 10px;
+    }
+  }
+
+  .registerBox, .socialForm {
+    padding: 30px;
+    border: solid 1px;
+    border-radius: 5px;
+  }
+
+  .socialForm {
+    h3 {
+      margin-bottom: 20px
+    }
+    .fbBox, .googleBox {
+      display: inline-block;
+      margin-right: 5px;
+      margin-left: 5px;
+      width: 80px;
+      height: 80px;  
+      background-size:cover; 
+    }
+    .fbBox{
+      background-image: url('/static/images/fb_icon.png');
+    }
+    .googleBox{
+      background-image: url('/static/images/google_icon.jpg');
+    }
+  }
+}
 </style>
